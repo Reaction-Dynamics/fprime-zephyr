a17dc97df68c92b8d19eadcfa4ec2380ab267e27
ecollin <etienne.collin@reactiondynamics.space>
2026-01-21 13:58:57 -0500

    style: formatting

--- fprime-zephyr/Drv/ZephyrAsyncUartDriver/ZephyrAsyncUartDriver.hpp
+++ fprime-zephyr/Drv/ZephyrAsyncUartDriver/ZephyrAsyncUartDriver.hpp
@@ -12,65 +12,60 @@
 #include "config/FwSizeTypeAliasAc.h"
 #include "fprime-zephyr/Drv/ZephyrAsyncUartDriver/ZephyrAsyncUartDriverComponentAc.hpp"
 
-#include <array>
-#include <atomic>
 #include <zephyr/device.h>
 #include <zephyr/drivers/uart.h>
 #include <zephyr/kernel.h>
 #include <zephyr/sys/ring_buffer.h>
+#include <array>
+#include <atomic>
 
 namespace Zephyr {
 
 class ZephyrAsyncUartDriver : public ZephyrAsyncUartDriverComponentBase {
+  public:
+    // ----------------------------------------------------------------------
+    // Construction, initialization, and destruction
+    // ----------------------------------------------------------------------
 
-public:
-  // ----------------------------------------------------------------------
-  // Construction, initialization, and destruction
-  // ----------------------------------------------------------------------
+    //! Construct object ZephyrAsyncUartDriver
+    //!
+    ZephyrAsyncUartDriver(const char* const compName /*!< The component name*/
+    );
 
-  //! Construct object ZephyrAsyncUartDriver
-  //!
-  ZephyrAsyncUartDriver(const char *const compName /*!< The component name*/
-  );
+    //! Destroy object ZephyrAsyncUartDriver
+    //!
+    ~ZephyrAsyncUartDriver();
 
-  //! Destroy object ZephyrAsyncUartDriver
-  //!
-  ~ZephyrAsyncUartDriver();
+    void configure(const struct device* dev, U32 baud_rate, FwSizeType rx_buffer_size);
 
-  void configure(const struct device* dev, U32 baud_rate, FwSizeType rx_buffer_size);
+  public:
+    // ----------------------------------------------------------------------
+    // Handler implementations for user-defined typed input ports
+    // ----------------------------------------------------------------------
 
-public:
-  // ----------------------------------------------------------------------
-  // Handler implementations for user-defined typed input ports
-  // ----------------------------------------------------------------------
+    //! Handler implementation for send
+    //!
+    void asyncSend_handler(const FwIndexType portNum,  //!< The port number
+                           Fw::Buffer& sendBuffer      //!< The buffer to send through UART
+    );
 
-  //! Handler implementation for send
-  //!
-  void
-  asyncSend_handler(const FwIndexType portNum, //!< The port number
-                    Fw::Buffer &sendBuffer //!< The buffer to send through UART
-  );
+    // Handler implementation for send
+    //!
+    Drv::ByteStreamStatus send_handler(const FwIndexType portNum,  //!< The port number
+                                       Fw::Buffer& sendBuffer      //!< The buffer to send through UART
+    );
 
-  // Handler implementation for send
-  //!
-  Drv::ByteStreamStatus
-  send_handler(const FwIndexType portNum, //!< The port number
-               Fw::Buffer &sendBuffer     //!< The buffer to send through UART
-  );
+    void recvReturnIn_handler(const FwIndexType portNum,  //!< The port number
+                              Fw::Buffer& returnBuffer    //!< The buffer being returned to its owner
+    );
 
-  void recvReturnIn_handler(
-      const FwIndexType portNum, //!< The port number
-      Fw::Buffer &returnBuffer   //!< The buffer being returned to its owner
-  );
-
-private:
-  void static uartEventCallback(const struct device *dev,
-                                struct uart_event *evt, void *user_data);
-  const struct device *m_dev;
-  Fw::Buffer m_rxBuff;
-  Fw::Buffer m_pendingTxBuff;
+  private:
+    void static uartEventCallback(const struct device* dev, struct uart_event* evt, void* user_data);
+    const struct device* m_dev;
+    Fw::Buffer m_rxBuff;
+    Fw::Buffer m_pendingTxBuff;
 };
 
-} // end namespace Zephyr
+}  // end namespace Zephyr
 
 #endif
